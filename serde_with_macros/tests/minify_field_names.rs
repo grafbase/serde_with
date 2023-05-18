bd2feebebc0502678e339d31707a084eeb929ff1
Jakub Wieczorek <jakub@grafbase.com>
2023-05-18 14:41:16 +0200

    Make more powerful.

--- serde_with_macros/tests/minify_field_names.rs
+++ serde_with_macros/tests/minify_field_names.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use serde_with_macros::minify_field_names;
 
-#[minify_field_names]
+#[minify_field_names(serialize = "minified", deserialize = "minified")]
 #[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
 struct StructWithLongFieldNames {
     long_field_name: usize,
@@ -13,15 +13,25 @@ struct StructWithLongFieldNames {
     preserved_field_name: usize,
 }
 
+#[minify_field_names(serialize = "intact", deserialize = "intact")]
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+struct StructWithLongFieldNames2 {
+    long_field_name: usize,
+    another_field_name: usize,
+    another_field_name_that_shares_a_prefix_with_the_previous_one: usize,
+    #[serde(rename = "preserved_field_name")]
+    preserved_field_name: usize,
+}
+
 #[test]
-fn test_minify_field_names() {
+fn test_minify_field_names2() {
     let expected = json!({
-        "l": 1,
-        "a": 2,
-        "A": 3,
+        "long_field_name": 1,
+        "another_field_name": 2,
+        "another_field_name_that_shares_a_prefix_with_the_previous_one": 3,
         "preserved_field_name": 4,
     });
-    let input = StructWithLongFieldNames {
+    let input = StructWithLongFieldNames2 {
         long_field_name: 1,
         another_field_name: 2,
         another_field_name_that_shares_a_prefix_with_the_previous_one: 3,
